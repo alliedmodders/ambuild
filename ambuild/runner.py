a3a3d71ea41b956ee5f440b5acc09612fc5eef7e
Scott Ehlert <ds@alliedmods.net>
2010-05-27 19:27:42 -0500

    Added ability to run specific jobs with build.py (bug 4421, r=dvander).
    Also added --list-jobs option to build.py which prints a list of all known jobs.

--- ambuild/runner.py
+++ ambuild/runner.py
@@ -16,6 +16,7 @@ class Runner:
 		self.options = OptionParser()
 		self.target = { }
 		self.numCPUs = osutil.NumberOfCPUs()
+		self.quietAdd = False
 		if osutil.IsWindows():
 			self.target['platform'] = 'windows'
 		elif sys.platform.startswith('linux'):
@@ -27,16 +28,42 @@ class Runner:
 		print(text)
 
 	def AddJob(self, name, workFolder = None):
-		print('Adding job {0}.'.format(name))
+		if not self.quietAdd:
+			print('Adding job {0}.'.format(name))
 		job = Job(self, name, workFolder)
 		self.jobs.append(job)
 		return job
+
+	def ListJobs(self):
+		print("Listing {0} jobs...".format(len(self.jobs)))
+		for job in self.jobs:
+			print(job.name)
+
+	def RunJobs(self, jobs):
+		for job in jobs:
+			print('Running job: {0}...'.format(job.name))
+			if job.workFolder != None:
+				workFolder = os.path.join(self.outputFolder, job.workFolder)
+				if not os.path.isdir(workFolder):
+					os.makedirs(workFolder)
+				osutil.PushFolder(workFolder)
+				try:
+					job.run(self)
+				except Exception as e:
+					print('Job failed: {0}'.format(str(e)))
+					sys.exit(1)
+				if job.workFolder != None:
+					osutil.PopFolder()
+				print('Completed job: {0}.'.format(job.name))
 
 	def CallerScript(self, num = 1):
 		return sys._getframe(num).f_code.co_filename
 
 	def Build(self):
 		self.mode = 'build'
+		(options, args) = self.options.parse_args()
+		argn = len(args)
+		self.quietAdd = options.list == True or argn > 0
 		self.outputFolder = os.path.abspath(os.getcwd())
 		cacheFolder = os.path.join(self.outputFolder, '.ambuild')
 		if not os.path.isdir(cacheFolder):
@@ -48,21 +75,24 @@ class Runner:
 		self.cache.LoadCache()
 		self.sourceFolder = self.cache['sourceFolder']
 		self.LoadFile(os.path.join(self.sourceFolder, 'AMBuildScript'))
-		for job in self.jobs:
-			print('Running job: {0}...'.format(job.name))
-			if job.workFolder != None:
-				workFolder = os.path.join(self.outputFolder, job.workFolder)
-				if not os.path.isdir(workFolder):
-					os.makedirs(workFolder)
-				osutil.PushFolder(workFolder)
-			try:
-				job.run(self)
-			except Exception as e:
-				print('Job failed: {0}'.format(str(e)))
-				sys.exit(1)
-			if job.workFolder != None:
-				osutil.PopFolder()
-			print('Completed job: {0}.'.format(job.name))
+		if options.list:
+			self.ListJobs()
+			return
+		jobs = []
+		if argn > 0:
+			# Run jobs in order specified on command line
+			for j in args:
+				validJob = False
+				for job in self.jobs:
+					if job.name == j:
+						jobs.append(job)
+						validJob = True
+						break
+				if not validJob:
+					raise Exception('{0} is not a valid job name'.format(j))
+		else:
+			jobs = self.jobs
+		self.RunJobs(jobs)
 
 	def Configure(self, folder):
 		self.mode = 'config'
@@ -88,6 +118,8 @@ import sys
 import ambuild.runner as runner
 
 run = runner.Runner()
+run.options.usage = '%prog [options] [job list]'
+run.options.add_option('-l', '--list-jobs', action='store_true', dest='list', help='print list of jobs')
 run.Build()
 		""")
 
