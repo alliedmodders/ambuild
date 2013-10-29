a84dd97abbf32d5899779ce826212947cf70175c
David Anderson <dvander@alliedmods.net>
2013-10-29 00:17:17 -0700

    Add ambuild2 to setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
-# vim: set ts=2 sw=2 tw=99 noet: 
+# vim: set ts=2 sw=2 tw=99 et: 
 
 from distutils.core import setup
 
-setup(name = 'AMBuild',
-	version = '0.1',
-	description = 'AlliedModders Build System',
-	author = 'David Anderson',
-	author_email = 'dvander@alliedmods.net',
-	url = 'http://www.alliedmods.net/',
-	packages = ['ambuild']
-	)
+setup(
+  name = 'AMBuild',
+  version = '2.0',
+  description = 'AlliedModders Build System',
+  author = 'David Anderson',
+  author_email = 'dvander@alliedmods.net',
+  url = 'http://www.alliedmods.net/ambuild',
+  packages = [
+    'ambuild',
+    'ambuild2',
+    'ambuild2.frontend',
+    'ambuild2.ipc',
+    'ambuild2.frontend.amb2'
+  ]
+)
 
 
