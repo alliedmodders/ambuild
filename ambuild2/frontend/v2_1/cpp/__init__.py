965f21beb316e9f693b5236a9b33903625d673f1
David Anderson <dvander@alliedmods.net>
2016-08-07 14:50:56 -0700

    Add "type" member to CppNodes.

--- ambuild2/frontend/v2_1/cpp/__init__.py
+++ ambuild2/frontend/v2_1/cpp/__init__.py
@@ -2,7 +2,8 @@ from ambuild2.frontend.v2_1.cpp.compiler import Compiler
 from ambuild2.frontend.v2_1.cpp.builders import Dep
 
 class CppNodes(object):
-  def __init__(self, output, debug_outputs):
+  def __init__(self, output, debug_outputs, type):
     self.binary = output
     self.debug = debug_outputs
+    self.type = type
 
