394af4e29bc56025c3e6d19495e7348264041d92
WildCard65 <lollol222gg@gmail.com>
2020-08-10 15:33:55 -0700

    Fix IsSubPath issues with relative paths.

--- ambuild2/frontend/paths_test.py
+++ ambuild2/frontend/paths_test.py
@@ -1,4 +1,5 @@
 # vim: set sts=4 ts=8 sw=4 tw=99 et: 
+import ntpath
 import unittest
 from ambuild2.frontend import paths
 
@@ -6,3 +7,6 @@ class IsSubPathTests(unittest.TestCase):
     def runTest(self):
         self.assertEqual(paths.IsSubPath("/a/b/c", "/a"), True)
         self.assertEqual(paths.IsSubPath("/t/b/c", "/a"), False)
+        self.assertEqual(paths.IsSubPath("t", "./"), True)
+        self.assertEqual(paths.IsSubPath(r"C:\blah", "C:\\", ntpath), True)
+        self.assertEqual(paths.IsSubPath(r"C:\blah", "D:\\", ntpath), False)
