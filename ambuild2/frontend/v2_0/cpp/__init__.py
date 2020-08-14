68aa315e84bab595dd37a79379b61ee9d5a4371c
David Anderson <dvander@alliedmods.net>
2020-08-14 00:18:13 -0700

    Expand the smoke test to add API invocations.

--- ambuild2/frontend/v2_0/cpp/__init__.py
+++ ambuild2/frontend/v2_0/cpp/__init__.py
@@ -1,7 +1,3 @@
 from ambuild2.frontend.v2_0.cpp.compilers import Compiler
+from ambuild2.frontend.v2_0.cpp.builders import CppNodes
 from ambuild2.frontend.v2_0.cpp.builders import Dep
-
-class CppNodes(object):
-    def __init__(self, output, debug_outputs):
-        self.binary = output
-        self.debug = debug_outputs
