alliedmodders/ambuild

commit 10571c0d3cde4546a9ee7fcbcb565412216e6f38

Generate compile_commands.json for Clangd (#173)

ambuild2/frontend/amb2_gen.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with AMBuild. If not, see <http://www.gnu.org/licenses/>.
 import os
+import json
 from ambuild2 import util
 from ambuild2 import nodetypes
 from ambuild2 import database
@@ -34,6 +35,7 @@ class Generator(BaseGenerator):
         self.had_symlink_fallback = False
         self.db = cm.db
         self.is_bootstrap = not self.db
+        self.compdb = []
 
     @property
     def backend(self):
@@ -131,6 +133,12 @@ class Generator(BaseGenerator):
         self.cleanup()
         self.db.commit()
         self.db.vacuum()
+
+        if self.cm.options.generate_compdb:
+            compile_commands = os.path.join(self.cm.buildPath, 'compile_commands.json')
+            with open(compile_commands, 'w') as fp:
+                json.dump(self.compdb, fp, indent=2)
+
         if self.is_bootstrap:
             self.saveVars()
             self.db.close()
@@ -751,6 +759,14 @@ class Generator(BaseGenerator):
         }
         if obj.dep_info:
             cxxData['deps'] = obj.dep_info
+
+        if self.cm.options.generate_compdb:
+            self.compdb.append({
+                "directory": obj.folderNode.path,
+                "arguments": obj.argv,
+                "file": obj.inputObj,
+                "output": obj.outputs[0]
+            })
 
         _, output_nodes = self.addCommand(context = cx,
                                           weak_inputs = obj.sourcedeps,

ambuild2/frontend/v2_2/prep.py
@@ -41,6 +41,12 @@ class Preparer(object):
                                   default = False,
                                   help = "List available build system generators, then exit.")
         self.options.add_argument(
+            "--generate-compile-commands",
+            action = "store_true",
+            dest = "generate_compdb",
+            default = True,
+            help = "Generate a JSON Compilation Database for Clangd.")
+        self.options.add_argument(
             "--make-scripts",
             action = "store_true",
             dest = "make_scripts",

commit a95acaa33d3225503ced1088841608a89fc7c841

Better fix for regression in #173

ambuild2/frontend/amb2_gen.py
@@ -134,7 +134,7 @@ class Generator(BaseGenerator):
         self.db.commit()
         self.db.vacuum()
 
-        if self.cm.options.generate_compdb:
+        if getattr(self.cm.options, 'generate_compdb', False):
             compile_commands = os.path.join(self.cm.buildPath, 'compile_commands.json')
             with open(compile_commands, 'w') as fp:
                 json.dump(self.compdb, fp, indent=2)
@@ -760,7 +760,7 @@ class Generator(BaseGenerator):
         if obj.dep_info:
             cxxData['deps'] = obj.dep_info
 
-        if self.cm.options.generate_compdb:
+        if getattr(self.cm.options, 'generate_compdb', False):
             self.compdb.append({
                 "directory": obj.folderNode.path,
                 "arguments": obj.argv,

ambuild2/frontend/v2_2/context_manager.py
@@ -37,7 +37,6 @@ class ContextManager(context_manager.ContextManager):
 
         # Detect the target architecture.
         self.host = System.Host
-        self.options.generate_compdb = getattr(self.options, 'generate_compdb', False)
 
     @property
     def apiVersion(self):